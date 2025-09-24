efee073575736589bb769d6b4107cda683ff0884
Sarah Tasnim Diya <sarahtasnim99@gmail.com>
2025-09-24 06:43:54 +0600

    directory dlt

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 model = None
 
